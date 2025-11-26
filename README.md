ac57883ec820f2059cf1d326e0e12868aa2e202a
chenliang <chenliang@nwd-ubuntu1804-vm>
2025-11-26 11:29:43 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fftw3-3.3.10-forAndroid
 Use the latest version of fftw to compile the Android static library warehouse
+
+# how to use?
+## step1
+    git clone xxxxxxxxxxxxxxxxxx.git
+
+## step2
+    run build.sh ,ndk will be automatically downloaded and configured
+
+## step3
+    The static library will be generated in the out_libs/ directory
+
+# hint
+    Single-precision, neon-accelerated static library used by default
---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+error_handler() {
+    echo "Error occurred in script at line: $1"
+    exit 1
+}
+
+android_ndk_detect(){
+    NDK_PAKEGE_URL="https://dl.google.com/android/repository/android-ndk-r27d-linux.zip?hl=zh-cn"
+    echo "Please set ANDROID_NDK_HOME to your Android NDK path."
+    echo "Automatically Set ANDROID_NDK_HOME by downloading NDK? (y/n)"
+    read answer
+    if [ "$answer" != "y" -a "$answer" != "Y" ]; then
+        return 1
+    fi
+    echo "Downloading NDK..."
+    wget -q "$NDK_PAKEGE_URL" -O android-ndk.zip
+    unzip -q android-ndk.zip -d $HOME
+    echo "export ANDROID_NDK_HOME=$HOME/android-ndk-r27d" >> $HOME/.bashrc
+    source $HOME/.bashrc
+    echo "NDK has been downloaded and configured."
+    return 0
+}
+
+trap 'error_handler $LINENO' ERR
+
+if [ -z "$ANDROID_NDK_HOME" ]; then
+    android_ndk_detect
+fi
+
+export TARGET=aarch64-linux-android
+if [ $1 -eq 32 ]; then
+    export TARGET=armv7a-linux-androideabi
+fi
+
+export API=21
+export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
+export CC=aarch64-linux-android$API-clang
+export CXX=aarch64-linux-android$API-clang++
+export AR=llvm-ar
+export RANLIB=llvm-ranlib
+
+INSTALL_DIR="`pwd`/out_libs/$TARGET"
+
+./configure --host=$TARGET \
+            --enable-float \
+            --enable-neon \
+            --prefix=$INSTALL_DIR \
+            --disable-doc \
+            CC=$CC \
+            CXX=$CXX \
+            AR=$AR \
+            RANLIB=$RANLIB \
+            CFLAGS="-O3 -fPIC" \
+            LDFLAGS="-static"
+
+make -j$(nproc)
+
+make install
+echo “”
+echo "################################################"
+echo "#Build and installation completed successfully.#"
+echo "################################################"
+exit 0
